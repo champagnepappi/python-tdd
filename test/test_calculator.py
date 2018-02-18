80b2f50f5edb288d06595dab1284365f2ad3f861
kevin <kevinochieng548@gmail.com>
2018-02-18 22:06:57 +0300

    define the setup method

--- test/test_calculator.py
+++ test/test_calculator.py
@@ -3,9 +3,11 @@ from app.calculator import Calculator
 
 class TddInPythonExample(unittest.TestCase):
 
+    def setUp(self):
+        self.calc = Calculator()
+
     def test_calculator_add_method_returns_correct_result(self):
-        calculator = Calculator()
-        result = calc.add(2,3)
+        result = self.calc.add(2,3)
         self.assertEqual(5, result)
 
 if __name__ == '__main__':
